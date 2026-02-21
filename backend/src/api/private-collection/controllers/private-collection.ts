10c3b5e9c2dffaf286d29d824982ed67153d9dd0
jaytcdj-dev <jayt.cdj@gmail.com>
2026-02-21 15:25:23 +0530

    Strapi removed

--- backend/src/api/private-collection/controllers/private-collection.ts
+++
@@ -1,7 +0,0 @@
-/**
- * private-collection controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::private-collection.private-collection');
--- backend/src/api/private-collection/services/private-collection.ts
+++
@@ -1,7 +0,0 @@
-/**
- * private-collection service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::private-collection.private-collection');
